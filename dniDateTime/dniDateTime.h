32d96ade94046ba6ee1c4d1a25896d40fca9b76a
Jeff Hitchcock <mail@command-center.org>
2016-09-22 16:24:07 -0700

    Initial Commit

---
+++ dniDateTime/dniDateTime.h
@@ -0,0 +1,19 @@
+//
+//  dniDateTime.h
+//  dniDateTime
+//
+//  Created by Jeff Hitchcock on 2016-09-22.
+//  Copyright © 2016 Jeff Hitchcock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for dniDateTime.
+FOUNDATION_EXPORT double dniDateTimeVersionNumber;
+
+//! Project version string for dniDateTime.
+FOUNDATION_EXPORT const unsigned char dniDateTimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dniDateTime/PublicHeader.h>
+
+
